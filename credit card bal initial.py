edf2170f65ec00d7e2d16420173c5fc70fdbbbbe
adwait <adwaitmathkari@gmail.com>
2019-04-14 16:48:30 +0530

    general

--- credit card bal initial.py
+++ credit card bal initial.py
@@ -18,9 +18,11 @@ def yearbalance(balance,annualInterestRate,monthlyPayment):
         itr+=1
         
     return balance
-	      
-monthlyPayment=0        
-while yearbalance(balance,annualInterestRate,monthlyPayment)>0:    
+        
+monthlyPayment=0  
+balance=10000
+annualInterestRate=11.32      
+while yearbalance(balance, annualInterestRate, monthlyPayment)>0:    
     monthlyPayment+=10
 print('Lowest Payment: ', monthlyPayment)
             
